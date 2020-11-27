8212c3ac3137290c68bbbaa195f6df2b72fd57d1
OndrejPalat <228728@vutbr.cz>
2020-11-27 21:57:24 +0100

    Set direction function, minor updated
    Adds function pioneer_set_direction for loading gps coordinates and turning to apropriate direction.
    Adds turning reactions to move_along function. Minor sensor setting in main controller.

--- controllers/pioneer3at_controll/pioneer3at_controll.m
+++ controllers/pioneer3at_controll/pioneer3at_controll.m
@@ -21,10 +21,13 @@ end
 velocity = 3;
 sensors_data = zeros(numel(sensors), 1);
 message = "";
-n_of_turns = 0;
+turn_requests = 0;
 n_of_alignments = 0;
-direction_vector = [0 0 1];
+direction_vector = [-1 0 1];
 reaction_distance = 900;
+time_set_direction = 0;
+gps_destination = [-10 20 -10];
+set_direction_request = 1;
 
 wheel_motors = zeros(4, 1);
 wheel_motors(1) = wb_robot_get_device('front left wheel');
@@ -35,47 +38,77 @@ for i = 1 : 4
   wb_motor_set_position(wheel_motors(i), inf);
   wb_motor_set_velocity(wheel_motors(i), velocity);
 end
+
 compass = wb_robot_get_device('compass');
 wb_compass_enable(compass, TIME_STEP);
+emitter = wb_robot_get_device('emitter');
+receiver = wb_robot_get_device('receiver');
+wb_receiver_enable(receiver, TIME_STEP);
+gps = wb_robot_get_device('gps');
+wb_gps_enable(gps, TIME_STEP);
 
 
 while wb_robot_step(TIME_STEP) ~= -1
+
   for i = 1 : numel(sensors)
     sensors_data(i) = wb_distance_sensor_get_value(sensors(i));
     message = message + "  " + string(sensors_data(i));
-  end  
+  end
+  
+  if wb_robot_get_time() > time_set_direction + 10
+    set_direction_request = 1;
+  end
+  
+  if set_direction_request > 0
+    pioneer_set_direction(gps_destination, 100, velocity,...
+                          wheel_motors, gps, compass, TIME_STEP)
+    time_set_direction = wb_robot_get_time();
+    set_direction_request = set_direction_request - 1;
+  end
+  
 %  disp("Sensors data:" + newline + message);
 %  message = "";
 %  disp(wb_compass_get_values(compass));
         
   
-  if n_of_turns > 0
-    pioneer_turn(80, "CW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
-    n_of_turns = n_of_turns - 1;
-  end
-  
-  if sensors_data(4) > reaction_distance || sensors_data(5) > reaction_distance
-    pioneer_turn(90, "CW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
+  if sensors_data(2) > reaction_distance
+    pioneer_turn(30, "CW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
   end
   if sensors_data(3) > reaction_distance
     pioneer_turn(45, "CW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
   end
+  if sensors_data(4) > reaction_distance || sensors_data(5) > reaction_distance
+    compass_vector = wb_compass_get_values(compass);
+    cross_product = cross(compass_vector, direction_vector);
+    if cross_product(2) < 0
+      turn_orientation = "CW";
+    else
+      turn_orientation = "CCW";
+    end
+    pioneer_turn(90, turn_orientation, 0.5, wheel_motors, compass,...
+                 velocity, TIME_STEP)
+  end
   if sensors_data(6) > reaction_distance
     pioneer_turn(45, "CCW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
-  end
-  if sensors_data(2) > reaction_distance
-    pioneer_turn(30, "CW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
   end
   if sensors_data(7) > reaction_distance
     pioneer_turn(30, "CCW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
   end
-  if sensors_data(8) > reaction_distance
+  
+  if sensors_data(8) > reaction_distance / 1.2
+    turn_requests = 0;
     pioneer_move_along(reaction_distance, "right", velocity, sensors,...
                      wheel_motors, compass, TIME_STEP)
-  end
-  if sensors_data(1) > reaction_distance
+    turn_orientation = "CW";
+    turn_requests = 6;
+  end  
+  if sensors_data(1) > reaction_distance / 1.2
+    turn_requests = 0;
     pioneer_move_along(reaction_distance, "left", velocity, sensors,...
                      wheel_motors, compass, TIME_STEP)
+    turn_orientation = "CCW";
+    turn_requests = 6;
+%    pioneer_turn(90, "CCW", 0.5, wheel_motors, compass, velocity, TIME_STEP)
   end
   
   if n_of_alignments > 0
@@ -84,6 +117,12 @@ while wb_robot_step(TIME_STEP) ~= -1
     n_of_alignments = n_of_alignments - 1;
   end
 
+  if turn_requests > 0
+    pioneer_turn(15, turn_orientation, 0, wheel_motors, compass,...
+                 velocity, TIME_STEP)
+    turn_requests = turn_requests - 1;
+  end
+  
   drawnow;
 
 end
--- controllers/pioneer3at_controll/pioneer_move_along.m
+++ controllers/pioneer3at_controll/pioneer_move_along.m
@@ -2,16 +2,19 @@ function pioneer_move_along(distance, left_right, velocity, sensors, wheel_motor
     
     front_sensors_buffer = NaN(10, 1);
     rear_sensors_buffer = NaN(10, 1);
-    side_sensors_idx = [];
+    sensors_idx = [];
     sensors_data = zeros(numel(sensors), 1);
+    start_of_function = true;
     if left_right == "left"
-        side_sensors_idx = [1, 16];
+        sensors_idx = [16, 1, 2, 3, 4, 5, 6, 7];
         motors_close = [wheel_motors(1), wheel_motors(3)];
         motors_far = [wheel_motors(2), wheel_motors(4)];
+        turn_orientation = "CW";
     elseif left_right == "right"
-        side_sensors_idx = [8, 9];
+        sensors_idx = [9, 8, 7, 6, 5, 4, 3, 2];
         motors_close = [wheel_motors(2), wheel_motors(4)];
         motors_far = [wheel_motors(1), wheel_motors(3)];
+        turn_orientation = "CCW";
     end
     
     while wb_robot_step(TIME_STEP) ~= -1
@@ -20,17 +23,49 @@ function pioneer_move_along(distance, left_right, velocity, sensors, wheel_motor
             sensors_data(i) = wb_distance_sensor_get_value(sensors(i));
         end        
         front_sensors_buffer = [front_sensors_buffer(1:end-1);...
-            sensors_data(side_sensors_idx(1))];
+            sensors_data(sensors_idx(2))];
         rear_sensors_buffer = [rear_sensors_buffer(1:end-1);...
-            sensors_data(side_sensors_idx(2))];                           
+            sensors_data(sensors_idx(1))];                           
 %         mean(rear_sensor_buffer(~isnan(rear_sensor_buffer)))
-
-
-        if mean(front_sensors_buffer(~isnan(front_sensors_buffer)))...
-                < distance / 2 || any(sensors_data(2:6) > distance)
+        
+        if start_of_function
             
+            start_of_function = false;           
+        end
+
+
+        
+        if sensors_data(sensors_idx(3)) > distance
+            pioneer_turn(30, turn_orientation, 0.5, wheel_motors,...
+                         compass, velocity, TIME_STEP)
+        end
+        if sensors_data(sensors_idx(4)) > distance
+            pioneer_turn(45, turn_orientation, 0.5, wheel_motors,...
+                         compass, velocity, TIME_STEP)
+        end
+        if sensors_data(sensors_idx(5)) > distance ||...
+                sensors_data(sensors_idx(6)) > distance
+            pioneer_turn(90, turn_orientation, 0.5, wheel_motors,...
+                         compass, velocity, TIME_STEP)
+        end
+        if sensors_data(sensors_idx(7)) > distance
+            pioneer_turn(135, turn_orientation, 0.5, wheel_motors,...
+                         compass, velocity, TIME_STEP)
+        end
+        if sensors_data(sensors_idx(8)) > distance
+            pioneer_turn(150, turn_orientation, 0.5, wheel_motors,...
+                         compass, velocity, TIME_STEP)
+        end
+        
+        if mean(front_sensors_buffer(~isnan(front_sensors_buffer))) < distance / 2            
             for i = 1 : numel(wheel_motors)
                 wb_motor_set_velocity(wheel_motors(i), velocity);
+            end
+            time = wb_robot_get_time();
+            while wb_robot_step(TIME_STEP) ~= -1
+                if wb_robot_get_time() > time + 3
+                    break
+                end
             end
             break
         end
